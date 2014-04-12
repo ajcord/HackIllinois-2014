b18e27526ca3ebc4a686056c557457735633bce6
bchong95 <bschong2@illinois.edu>
2014-04-11 23:07:54 -0500

    Created Place and Coordinate Class

---
+++ HackIllinois/src/com/example/hackillinois/Place.java
@@ -0,0 +1,17 @@
+package com.example.hackillinois;
+
+import android.R.bool;
+import android.R.string;
+
+public class Place 
+{
+	private string name;
+	private bool hasBeen;
+	private Coordinate coords;
+	private string description;
+	private string hint;
+	
+	
+	
+	
+}
